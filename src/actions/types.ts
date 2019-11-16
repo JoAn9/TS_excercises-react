6703f36d3d1fe6edff01d006ee683fc194b00a9f
JoAn <joannacalinska@gmail.com>
2019-11-16 21:55:28 +0100

    add type Action and new action delete

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,3 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   fetchTodos, // (default: fetchTodos = 0)
+  deleteTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
