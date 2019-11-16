e20ad389c476de1f9c34b4ef06f0e6a63f1487bb
JoAn <joannacalinska@gmail.com>
2019-11-16 21:56:01 +0100

    reducer to delete action

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,13 +1,13 @@
-import {Todo, FetchTodosAction } from '../actions';
-import { ActionTypes } from '../actions/types'
+import {Todo, Action, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   const { type, payload } = action;
 
   switch(type) {
     case ActionTypes.fetchTodos:
       return payload;
-
+    case ActionTypes.deleteTodo:
+      return state.filter((item: Todo) => item.id !== payload);
     default:
       return state;
   }
