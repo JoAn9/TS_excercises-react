726f3905a3c6ec3145162267e848d4a0c05c0b59
JoAn <joannacalinska@gmail.com>
2019-11-18 20:51:05 +0100

    add delete button to app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
-const _App = ({ todos, fetchTodos }: AppProps): JSX.Element => {
+const _App = (props: AppProps): JSX.Element => {
+  const { todos, fetchTodos, deleteTodo } = props;
   const onButtonClick = (): void => {
     fetchTodos();
   };
 
+  const onDeleteClick = (id: number): void => {
+    deleteTodo(id);
+  };
+
   const renderList: JSX.Element[] = todos.map((item: Todo) => (
-    <li key={item.id}>{item.title}</li>
+    <li key={item.id}>
+      {item.title}{' '}
+      <button onClick={() => onDeleteClick(item.id)}>Delete</button>
+    </li>
   ));
 
   return (
@@ -29,4 +38,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
